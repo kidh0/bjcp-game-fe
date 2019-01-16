d214b0d1617e7db3272bce9d57c1b492be0f8740
Henrique Boaventura <hboavent@thoughtworks.com>
2019-01-16 15:19:59 -0200

    adding the basic content to the react frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import bjcp from './bjcp.png';
+import loader from './loader.svg';
 import './App.css';
 
+function Intro(props) {
+  return (
+    <div className="intro">
+      <h1>BJCP Game</h1>
+      <figure>
+        <img src={bjcp} alt="BJCP Logo" />
+      </figure>
+      <p>
+        This is a game to exercise BJCP judge knowledge through a simple multi answer quiz
+        <button className="btn" onClick={props.onClick} >Let's do it!</button>
+      </p>
+      <small className="foot-note">This game is no affiliated with BJCP</small>
+    </div>  
+  );
+}
+
+function Game() {
+  return (
+    <div className="wrapper">
+      <div className="options" id="option-1"></div>
+      <div className="options" id="option-2"></div>
+      <div className="options" id="option-3"></div>
+      <div className="options" id="option-4"></div>
+      <div className="info"></div>
+      <div className="points">0</div>
+      <div className="loader">
+        <img src={loader} alt="loader" />
+      </div>
+    </div>
+  )
+}
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.handleShowGameClick = this.handleShowGameClick.bind(this);
+    this.state = { showGame: false };
+  }
+
+  handleShowGameClick() {
+    this.setState({showGame: true})
+  }
+
   render() {
+    const showGame = this.state.showGame;
+    let content;
+
+    if (showGame) {
+      content = <Game />
+    } else {
+      content = <Intro onClick={this.handleShowGameClick} />
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="wrapper">
+        {content}
       </div>
     );
   }
