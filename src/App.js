2ec5fd742c27e636b9c3d7de1b6a5db6a09c2d0e
Henrique Boaventura <hboavent@thoughtworks.com>
2019-01-17 14:43:45 -0200

    fixing the game logic, now is time to refactor

--- src/App.js
+++ src/App.js
@@ -19,27 +19,55 @@ function Intro(props) {
   );
 }
 
-function Game() {
+function Option({id, answer, onClick, ...rest }) {
+  return (
+    <div onClick={() => onClick(answer)} className="options" id={`option-${id}`} {...rest} />
+  )
+}
+
+function Game(props) {
+  const question = props.data
+  const points = props.points
   return (
     <div className="wrapper">
-      <div className="options" id="option-1"></div>
-      <div className="options" id="option-2"></div>
-      <div className="options" id="option-3"></div>
-      <div className="options" id="option-4"></div>
-      <div className="info"></div>
-      <div className="points">0</div>
-      <div className="loader">
+      <Option id="1" answer={question.options[0].id} onClick={props.onClick}>{question.options[0].name}</Option>
+      <Option id="2" answer={question.options[1].id} onClick={props.onClick}>{question.options[1].name}</Option>
+      <Option id="3" answer={question.options[2].id} onClick={props.onClick}>{question.options[2].name}</Option>
+      <Option id="4" answer={question.options[3].id} onClick={props.onClick}>{question.options[3].name}</Option>
+      <div className="info">{question.type} : {question.content}</div>
+      <div className="points">Points: {points}</div>
+      {/* <div className="loader">
         <img src={loader} alt="loader" />
-      </div>
+      </div> */}
     </div>
   )
 }
 
 class App extends Component {
+  
   constructor(props) {
     super(props);
     this.handleShowGameClick = this.handleShowGameClick.bind(this);
-    this.state = { showGame: false };
+    this.handleAnswerClick = this.handleAnswerClick.bind(this);
+    this.state = { showGame: false, points: 0 }
+  }
+
+  componentDidMount() {
+    this.loadRemoteData();
+  }
+
+  loadRemoteData() {
+    fetch('https://bjcp-game-be.herokuapp.com/next')
+      .then(response => response.json())
+      .then(data => this.setState({ data: data }));
+  }
+
+  handleAnswerClick(answer) {
+    const points = this.state.points;
+    if(answer == this.state.data.answer) {
+      this.setState({points: points+1})
+    }
+    this.loadRemoteData();
   }
 
   handleShowGameClick() {
@@ -51,7 +79,7 @@ class App extends Component {
     let content;
 
     if (showGame) {
-      content = <Game />
+      content = <Game onClick={this.handleAnswerClick} points={this.state.points} data={this.state.data} />
     } else {
       content = <Intro onClick={this.handleShowGameClick} />
     }
