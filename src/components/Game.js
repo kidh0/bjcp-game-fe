a26cc65eb8138a7411425b97bde1340d487d68ad
Henrique Boaventura <hboavent@thoughtworks.com>
2019-01-24 13:09:56 -0200

    removing the classes from stateless components https://itnext.io/react-component-class-vs-stateless-component-e3797c7d23ab

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,29 +2,31 @@ import React from 'react';
 import Option from './Option';
 import loader from '../loader.svg';
 
-class Game extends React.Component {
-
-    render() {
-        const question = this.props.data
-        const points = this.props.points
-        const loading = this.props.loading
-        const onClickMethod = this.props.onClick
-        return (
-            <div className="wrapper">
-                {question.options.map(option => (                     
-                    <Option 
-                        answer={option.id} onClick={onClickMethod}>{option.name}</Option>    
-                ))}
-                <div className="info"><strong>{question.type}</strong>: {question.content}</div>
-                <div className="score">Score: {points}</div>
-                {loading &&
-                    <div className="loader">
-                        <img src={loader} alt="loader" />
-                    </div>
-                }
-            </div>
-        )
-    }
+const Game = (props) => {
+    const question = props.data
+    const points = props.points
+    const loading = props.loading
+    const onClickMethod = props.onClick
+    return (
+        <div className="wrapper">
+            {question.options.map((option,key) => (                     
+                <Option
+                    key={key} 
+                    answer={option.id} 
+                    onClick={onClickMethod}
+                >
+                    {option.name}
+                </Option>    
+            ))}
+            <div className="info"><strong>{question.type}</strong>: {question.content}</div>
+            <div className="score">Score: {points}</div>
+            {loading &&
+                <div className="loader">
+                    <img src={loader} alt="loader" />
+                </div>
+            }
+        </div>
+    )
 }
 
 export default Game;
--- src/components/Option.js
+++ src/components/Option.js
@@ -1,13 +1,10 @@
 import React from 'react';
 
-class Option extends React.Component {
-
-    render() {
-        let { onClick, answer, ...rest } = this.props;
-        return (
-            <div onClick={() => onClick(answer)} className="options" {...rest} />
-        )
-    }
+Option = (props) => {
+    let { onClick, answer, ...rest } = props;
+    return (
+        <div onClick={() => onClick(answer)} className="options" {...rest} />
+    )
 }
 
 export default Option;
